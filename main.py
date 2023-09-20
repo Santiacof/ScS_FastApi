97427d7137deca53cb345854bb6bde182d8f13c3
ScS <72279885+Santiacof@users.noreply.github.com>
2023-09-20 15:56:03 -0500

    feat: crate a new rout for a list to products

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+products = [
+  {
+  "id": 1,
+  "name": "Product 1",
+  "price": 20,
+  "stock": 10 
+  }
+]
+  
 @app.get("/")
 def message():
   return "Hello World" # Usar uvicorn main:app
   return "añadir /docs a la ruta muestra documentacion"
+
+@app.get("/products")
+def get_products():
+  return products # retornar listado
